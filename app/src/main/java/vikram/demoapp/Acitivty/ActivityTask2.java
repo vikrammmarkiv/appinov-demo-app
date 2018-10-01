27b043e2a3c1f2f87b912abe81b2641de2611a25
Vikram <vikrammmarkiv@gmail.com>
2018-10-02 00:31:25 +0530

    prompt to turn on location if disabled

--- app/src/main/java/vikram/demoapp/Acitivty/ActivityTask2.java
+++ app/src/main/java/vikram/demoapp/Acitivty/ActivityTask2.java
@@ -3,6 +3,7 @@ package vikram.demoapp.Acitivty;
 import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
@@ -16,15 +17,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import vikram.demoapp.R;
 
@@ -32,11 +43,12 @@ public class ActivityTask2 extends AppCompatActivity implements OnMapReadyCallba
 
     private FusedLocationProviderClient mFusedLocationClient;
     private GoogleMap map;
-    private static final int REQUEST_GPS = 10;
+    private static final int REQUEST_GPS = 10, REQUEST_CHECK_SETTINGS = 11;
     private FloatingActionButton shareLocation;
     private Location mlocation;
     private Dialog DOProgress;
     private BottomNavigationView navigation;
+    private GoogleApiClient mGoogleApiClient;
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -77,14 +89,12 @@ public class ActivityTask2 extends AppCompatActivity implements OnMapReadyCallba
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
         shareLocation = findViewById(R.id.shareLocation);
         shareLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String uri = "http://www.google.com/maps/place/"+mlocation.getLatitude()+","+mlocation.getLongitude();
+                String uri = "http://www.google.com/maps/place/" + mlocation.getLatitude() + "," + mlocation.getLongitude();
                 Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
                 sharingIntent.setType("text/plain");
                 String ShareSub = "Here is my location";
@@ -98,7 +108,7 @@ public class ActivityTask2 extends AppCompatActivity implements OnMapReadyCallba
         DOProgress = new Dialog(this);
         DOProgress.setContentView(R.layout.dialog_progress_bar);
         DOProgress.setCancelable(false);
-        if (DOProgress.getWindow()!=null)
+        if (DOProgress.getWindow() != null)
             DOProgress.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
         DOProgress.show();
 
@@ -106,8 +116,7 @@ public class ActivityTask2 extends AppCompatActivity implements OnMapReadyCallba
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_GPS);
             }
-        }
-        else getlocation();
+        } else checklocation();
 
 
     }
@@ -117,38 +126,13 @@ public class ActivityTask2 extends AppCompatActivity implements OnMapReadyCallba
         this.map = map;
     }
 
-    void getlocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null) {
-                            mlocation = location;
-                            map.addMarker(new MarkerOptions()
-                                    .position(new LatLng(location.getLatitude(),location.getLongitude()))
-                                    .title("Current Location"));
-                            new Handler().postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    DOProgress.dismiss();
-                                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mlocation.getLatitude(),mlocation.getLongitude()), 15.5f), 4000, null);
-                                }
-                            },2500);
-                        }
-                    }
-                });
-    }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case REQUEST_GPS: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getlocation();
+                    checklocation();
                 } else {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                         requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_GPS);
@@ -159,8 +143,89 @@ public class ActivityTask2 extends AppCompatActivity implements OnMapReadyCallba
     }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         navigation.setSelectedItemId(R.id.task2);
+        checklocation();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mGoogleApiClient!=null)
+            mGoogleApiClient.disconnect();
+    }
+
+    private void checklocation() {
+        final LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(60000);
+        locationRequest.setFastestInterval(20000);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        Task<LocationSettingsResponse> result =
+                LocationServices.getSettingsClient(this).checkLocationSettings(builder.build());
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(Task<LocationSettingsResponse> task) {
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    mGoogleApiClient = new GoogleApiClient.Builder(ActivityTask2.this)
+                            .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                                @Override
+                                public void onConnected(Bundle bundle) {
+                                    if (ActivityCompat.checkSelfPermission(ActivityTask2.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ActivityTask2.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                        return;
+                                    }
+                                    mFusedLocationClient.requestLocationUpdates(locationRequest,
+                                            new LocationCallback(){
+                                                @Override
+                                                public void onLocationResult(LocationResult locationResult) {
+                                                    if (locationResult != null) {
+                                                        mlocation = locationResult.getLastLocation();
+                                                        map.clear();
+                                                        map.addMarker(new MarkerOptions()
+                                                                .position(new LatLng(mlocation.getLatitude(), mlocation.getLongitude()))
+                                                                .title("Current Location"));
+                                                        new Handler().postDelayed(new Runnable() {
+                                                            @Override
+                                                            public void run() {
+                                                                DOProgress.dismiss();
+                                                                map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mlocation.getLatitude(), mlocation.getLongitude()), 15.5f), 4000, null);
+                                                            }
+                                                        },2000);
+                                                    }
+                                                };
+
+                                            },null);
+                                }
+                                @Override
+                                public void onConnectionSuspended( int i ){
+
+                                }
+
+                            })
+                            .addApi(LocationServices.API)
+                            .build();
+                    mGoogleApiClient.connect();
+                } catch (ApiException exception) {
+                    switch (exception.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            try {
+                                ResolvableApiException resolvable = (ResolvableApiException) exception;
+                                resolvable.startResolutionForResult(
+                                        ActivityTask2.this,
+                                        REQUEST_CHECK_SETTINGS);
+                            } catch (IntentSender.SendIntentException e) {
+                                // Ignore the error.
+                            } catch (ClassCastException e) {
+                                // Ignore, should be an impossible error.
+                            }
+                            break;
+                    }
+                }
+            }
+        });
     }
 }
